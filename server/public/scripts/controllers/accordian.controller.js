5f70c9f409c2f65479d3180bd42ef726936f741d
Christine Pogatchnik <christinepogatchnik@gmail.com>
2017-03-25 22:47:01 -0500

    updates

--- server/public/scripts/controllers/accordian.controller.js
+++ server/public/scripts/controllers/accordian.controller.js
@@ -30,8 +30,9 @@ app.controller('AccordionDemoCtrl', ['ClientsFactory', function(ClientsFactory){
   self.status = {
     isCustomHeaderOpen: false,
     isFirstOpen: true,
-    isFirstDisabled: false
+    isFirstDisabled: false,
   };
+
 
 
   ////sweet alerts
@@ -68,33 +69,3 @@ self.checkConfirm=function(){
 
 
 }]);
-
-
-// app.controller('AccordionDemoCtrl', function () {
-//   var self = this;
-//   self.oneAtATime = true;
-//
-//   self.groups = [
-//     {
-//       title: 'Dynamic Group Header - 1',
-//       content: 'Dynamic Group Body - 1'
-//     },
-//     {
-//       title: 'Dynamic Group Header - 2',
-//       content: 'Dynamic Group Body - 2'
-//     }
-//   ];
-//
-//   self.items = ['Item 1', 'Item 2', 'Item 3'];
-//
-//   self.addItem = function() {
-//     var newItemNo = self.items.length + 1;
-//     self.items.push('Item ' + newItemNo);
-//   };
-//
-//   self.status = {
-//     isCustomHeaderOpen: false,
-//     isFirstOpen: true,
-//     isFirstDisabled: false
-//   };
-// });
