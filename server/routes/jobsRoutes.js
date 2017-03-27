6bcf20733e83b4e91f753f149259acebdc9ae7dc
Christine Pogatchnik <christinepogatchnik@gmail.com>
2017-03-26 22:46:35 -0500

    selects in progress

--- server/routes/jobsRoutes.js
+++ server/routes/jobsRoutes.js
@@ -6,7 +6,7 @@ var pool = require('../config/database-pool.js'); // Creates database pool, if y
 router.get('/', function (req, res) {
   pool.connect()
     .then(function (client) {
-      client.query('SELECT jobs.*, clients.name FROM clients INNER JOIN jobs ON clients.id=jobs.client_id')
+      client.query('SELECT jobs.*, clients.name, jobs.id FROM clients FULL OUTER JOIN jobs ON clients.id=jobs.client_id')
         .then(function (result) {
           client.release();
           res.send(result.rows);
@@ -15,6 +15,8 @@ router.get('/', function (req, res) {
           console.log('error on SELECT', err);
           res.sendStatus(500);
         });
+      }).catch(function(err) {
+        console.log('error connecting to database:', err);
     });
 });
 
@@ -25,7 +27,7 @@ router.post('/', function (req, res) {
   pool.connect()
     .then(function (result) {
       client.query('INSERT INTO jobs (status , task_1, task_2, tasks_cost, materials, materials_cost, total_cost, date, client_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
-        [newJob.status, newJob.task_1, newJob.task_2, newJob.tasks_cost, newJob.materials, newJob.materials_cost, newJob.date, newJob.client_id])
+        [newJob.status, newJob.task_1, newJob.task_2, newJob.tasks_cost, newJob.materials, newJob.materials_cost, newJob.total_cost, newJob.date, newJob.client_id])
         .then(function (result) {
           client.release();
           res.sendStatus(201);
@@ -63,8 +65,8 @@ router.put('/update/:id', function(req, res) {
   console.log('Updating job:, ', jobObject);
   pool.connect()
     .then(function (client) {
-      client.query('UPDATE jobs SET status=$1, task_1=$2, task_2=$3, tasks_cost=$4, materials=$5, materials_cost=$6, total_cost=$7, date=$8, client_id=$9 WHERE id = $10',
-        [jobObject.status, jobObject.task_1, jobObject.task_2, jobObject.tasks_cost, jobObject.materials, jobObject.materials_cost, jobObject.date, jobObject.client_id, jobId])
+      client.query('UPDATE jobs SET status=$1, task_1=$2, task_2=$3, tasks_cost=$4, materials=$5, materials_cost=$6, total_cost=$7, date=$8, client_id=$9 WHERE id = $10 RETURNING *',
+        [jobObject.status, jobObject.task_1, jobObject.task_2, jobObject.tasks_cost, jobObject.materials, jobObject.materials_cost, jobObject.total_cost, jobObject.date, jobObject.client_id, jobId])
         .then(function (result) {
           console.log(result.rows);
           client.release();
