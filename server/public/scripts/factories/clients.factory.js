80b57761c717e7d7e3172b412209a694a846ee50
Christine Pogatchnik <christinepogatchnik@gmail.com>
2017-03-27 13:13:11 -0500

    sweet alert working

--- server/public/scripts/factories/clients.factory.js
+++ server/public/scripts/factories/clients.factory.js
@@ -1,51 +1,63 @@
 app.factory('ClientsFactory', ['$http', function($http){
-    console.log('Clients Factory Loaded');
+  console.log('Clients Factory Loaded');
 
 
-    var clients = { list: [] };
+  var clients = { list: [] };
 
-getClients();
+  getClients();
 
-function getClients(){
-  $http({
-    method: 'GET',
-    url: '/clients'
-  }).then(function(response){
-    console.log(response.data);
-    clients.list = response.data;
-  });
-}//end function
+  function getClients(){
+    $http({
+      method: 'GET',
+      url: '/clients'
+    }).then(function(response){
+      console.log(response.data);
+      clients.list = response.data;
+    });
+  }//end function
 
-function addClient(newClient) {
-  $http.post('/clients', newClient).then(function(response){
-    getClients();
-    console.log(response);
-  });
-}
+  function addClient(newClient) {
+    $http.post('/clients', newClient).then(function(response){
+      getClients();
+      console.log(response);
+    });
+  }
 
-function deleteClient(clientId) {
-  console.log(clientId);
-  $http.delete('/clients/' + clientId).then(function(response){
-    getClients();
+  function deleteClient(clientId) {
+    console.log(clientId);
+    swal({
+      title: "Are you sure?",
+      text: "You will not be able to recover this imaginary file!",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Yes, delete it!",
+      closeOnConfirm: false
+    }, function(){
+      $http.delete('/clients/' + clientId).then(function(response){
+        getClients();
+        swal("Deleted!", "Your imaginary file has been deleted.", "success");
 
-  });
-}
+      });
+    })
+  }
 
-function saveClient(client) {
-  console.log('client being saved:', client);
-  console.log('client id being save:', client.id);
-  $http.put('/clients/update/' + client.id, client).then(function(response){
-    getClients();
-    console.log('response', response);
+  function saveClient(client) {
+    console.log('client being saved:', client);
+    console.log('client id being save:', client.id);
+    $http.put('/clients/update/' + client.id, client).then(function(response){
+      getClients();
+      swal("Saved!");
+      console.log('response', response);
 
-  });
-}
+    });
+  }
 
-return {
- clients: clients,
- addClient: addClient,
- deleteClient: deleteClient,
- saveClient: saveClient,
-}
+  return {
+    clients: clients,
+    addClient: addClient,
+    deleteClient: deleteClient,
+    saveClient: saveClient,
+  }
 
 }]);
--- server/public/scripts/factories/jobs.factory.js
+++ server/public/scripts/factories/jobs.factory.js
@@ -1,51 +1,62 @@
 app.factory('JobsFactory', ['$http', function($http){
-    console.log('Jobs Factory Loaded');
+  console.log('Jobs Factory Loaded');
 
 
-    var jobs = { list: [] };
+  var jobs = { list: [] };
 
-getJobs();
+  getJobs();
 
-function getJobs(){
-  $http({
-    method: 'GET',
-    url: '/jobs'
-  }).then(function(response){
-    console.log(response.data);
-    jobs.list = response.data;
-  });
-}//end function
+  function getJobs(){
+    $http({
+      method: 'GET',
+      url: '/jobs'
+    }).then(function(response){
+      console.log(response.data);
+      jobs.list = response.data;
+    });
+  }//end function
 
-function addJob(newJob) {
-  $http.post('/jobs', newJob).then(function(response){
-    getJobs();
-    console.log(response);
-  });
-}
+  function addJob(newJob) {
+    $http.post('/jobs', newJob).then(function(response){
+      getJobs();
+      console.log(response);
+    });
+  }
 
-function deleteJob(jobId) {
-  console.log(jobId);
-  $http.delete('/jobs/' + jobId).then(function(response){
-    getJobs();
+  function deleteJob(jobId) {
+    console.log(jobId);
+    swal({
+      title: "Are you sure?",
+      text: "You will not be able to recover this imaginary file!",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#DD6B55",
+      confirmButtonText: "Yes, delete it!",
+      closeOnConfirm: false
+    }, function(){
+      $http.delete('/jobs/' + jobId).then(function(response){
+        getJobs();
+        swal("Deleted!", "Your imaginary file has been deleted.", "success");
+      });
+    })
+  }
 
-  });
-}
+  function saveJob(job) {
+    console.log('job being saved:', job);
+    console.log('job id being save:', job.id);
+    $http.put('/jobs/update/' + job.id, job).then(function(response){
+      getJobs();
+      swal("Saved!");
+      console.log('response', response);
 
-function saveJob(job) {
-  console.log('job being saved:', job);
-  console.log('job id being save:', job.id);
-  $http.put('/jobs/update/' + job.id, job).then(function(response){
-    getJobs();
-    console.log('response', response);
+    });
+  }
 
-  });
-}
-
-return {
- jobs: jobs,
- addJob: addJob,
- deleteJob: deleteJob,
- saveJob: saveJob,
-}
+  return {
+    jobs: jobs,
+    addJob: addJob,
+    deleteJob: deleteJob,
+    saveJob: saveJob,
+  }
 
 }]);
